ce55aa80b1b4582bb086d60bf603c5280f864e11
Robin Freyler <robin.freyler@gmail.com>
2024-05-03 20:23:32 +0200

    rename some Wasm bindings

--- benches/wasmi.rs
+++ benches/wasmi.rs
@@ -381,11 +381,11 @@ fn run_fib_recursive(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
     if !vm.test_filter().fib_recursive {
         return;
     }
-    static FIB_REC_WAT: &[u8] = include_bytes!("../res/wat/fib.recursive.wat");
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.recursive.wat");
     let name = vm.name();
     let id = format!("fib/recursive/{name}/{input}");
     c.bench_function(&id, |b| {
-        let wasm = wat2wasm(FIB_REC_WAT);
+        let wasm = wat2wasm(WASM);
         let mut runtime = vm.load(&wasm[..]);
         b.iter(|| {
             runtime.call(input);
@@ -404,11 +404,11 @@ fn run_fib_iterative(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
     if !vm.test_filter().fib_iterative {
         return;
     }
-    static FIB_REC_WAT: &[u8] = include_bytes!("../res/wat/fib.iterative.wat");
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.iterative.wat");
     let name = vm.name();
     let id = format!("fib/iterative/{name}/{input}");
     c.bench_function(&id, |b| {
-        let wasm = wat2wasm(FIB_REC_WAT);
+        let wasm = wat2wasm(WASM);
         let mut runtime = vm.load(&wasm[..]);
         b.iter(|| {
             runtime.call(input);
@@ -427,11 +427,11 @@ fn run_fib_tailrec(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
     if !vm.test_filter().fib_tailrec {
         return;
     }
-    static FIB_REC_WAT: &[u8] = include_bytes!("../res/wat/fib.tailrec.wat");
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.tailrec.wat");
     let name = vm.name();
     let id = format!("fib/tailrec/{name}/{input}");
     c.bench_function(&id, |b| {
-        let wasm = wat2wasm(FIB_REC_WAT);
+        let wasm = wat2wasm(WASM);
         let mut runtime = vm.load(&wasm[..]);
         b.iter(|| {
             runtime.call(input);
@@ -450,11 +450,11 @@ fn run_primes(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
     if !vm.test_filter().primes {
         return;
     }
-    static FIB_REC_WAT: &[u8] = include_bytes!("../res/wat/primes.wat");
+    static WASM: &[u8] = include_bytes!("../res/wat/primes.wat");
     let name = vm.name();
     let id = format!("primes/{name}/{input}");
     c.bench_function(&id, |b| {
-        let wasm = wat2wasm(FIB_REC_WAT);
+        let wasm = wat2wasm(WASM);
         let mut runtime = vm.load(&wasm[..]);
         b.iter(|| {
             runtime.call(input);
