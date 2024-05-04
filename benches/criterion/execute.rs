f65fd0810a4de7676e6fae1ba603331856ce109b
Robin Freyler <robin.freyler@gmail.com>
2024-05-04 09:12:23 +0200

    move benches into execute submodule

---
+++ benches/criterion/execute.rs
@@ -0,0 +1,143 @@
+use crate::utils::{wat2wasm, TestFilter};
+use crate::vms;
+use crate::vms::{BenchRuntime, BenchVm};
+use criterion::{criterion_group, criterion_main, Bencher, Criterion};
+use std::time::Duration;
+
+fn run_counter(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().counter {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/counter.wat");
+    let name = vm.name();
+    let id = format!("execute/counter/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_counter(c: &mut Criterion) {
+    const INPUT: i64 = 1_000_000;
+    for vm in vms() {
+        run_counter(c, &*vm, INPUT);
+    }
+}
+
+fn run_fib_recursive(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().fib_recursive {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.recursive.wat");
+    let name = vm.name();
+    let id = format!("execute/fib/recursive/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_fib_recursive(c: &mut Criterion) {
+    const INPUT: i64 = 30;
+    for vm in vms() {
+        run_fib_recursive(c, &*vm, INPUT);
+    }
+}
+
+fn run_fib_iterative(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().fib_iterative {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.iterative.wat");
+    let name = vm.name();
+    let id = format!("execute/fib/iterative/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_fib_iterative(c: &mut Criterion) {
+    const INPUT: i64 = 2_000_000;
+    for vm in vms() {
+        run_fib_iterative(c, &*vm, INPUT);
+    }
+}
+
+fn run_fib_tailrec(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().fib_tailrec {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/fib.tailrec.wat");
+    let name = vm.name();
+    let id = format!("execute/fib/tailrec/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_fib_tailrec(c: &mut Criterion) {
+    const INPUT: i64 = 1_000_000;
+    for vm in vms() {
+        run_fib_tailrec(c, &*vm, INPUT);
+    }
+}
+
+fn run_primes(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().primes {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/primes.wat");
+    let name = vm.name();
+    let id = format!("execute/primes/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_primes(c: &mut Criterion) {
+    const INPUT: i64 = 1_000;
+    for vm in vms() {
+        run_primes(c, &*vm, INPUT);
+    }
+}
+
+fn run_matrix_multiply(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
+    if !vm.test_filter().matrix_multiply {
+        return;
+    }
+    static WASM: &[u8] = include_bytes!("../res/wat/matrix-multiplication.wat");
+    let name = vm.name();
+    let id = format!("execute/matmul/{name}/{input}");
+    c.bench_function(&id, |b| {
+        let wasm = wat2wasm(WASM);
+        let mut runtime = vm.load(&wasm[..]);
+        b.iter(|| {
+            runtime.call(input);
+        });
+    });
+}
+
+pub fn bench_matrix_multiply(c: &mut Criterion) {
+    const INPUT: i64 = 200;
+    for vm in vms() {
+        run_matrix_multiply(c, &*vm, INPUT);
+    }
+}
--- benches/criterion/main.rs
+++ benches/criterion/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+mod execute;
 mod utils;
 mod vms;
 
@@ -15,12 +16,12 @@ criterion_group!(
         .measurement_time(Duration::from_millis(2000))
         .warm_up_time(Duration::from_millis(1000));
     targets =
-        bench_counter,
-        bench_fib_recursive,
-        bench_fib_iterative,
-        bench_fib_tailrec,
-        bench_primes,
-        bench_matrix_multiply,
+        execute::bench_counter,
+        execute::bench_fib_recursive,
+        execute::bench_fib_iterative,
+        execute::bench_fib_tailrec,
+        execute::bench_primes,
+        execute::bench_matrix_multiply,
 );
 criterion_main!(bench_wasmi);
 
@@ -54,142 +55,4 @@ fn vms() -> Vec<Box<dyn BenchVm>> {
             compiler: vms::wasmer::WasmerCompiler::Singlepass,
         }),
     ]
-}
-
-fn run_counter(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().counter {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/counter.wat");
-    let name = vm.name();
-    let id = format!("execute/counter/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_counter(c: &mut Criterion) {
-    const INPUT: i64 = 1_000_000;
-    for vm in vms() {
-        run_counter(c, &*vm, INPUT);
-    }
-}
-
-fn run_fib_recursive(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().fib_recursive {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/fib.recursive.wat");
-    let name = vm.name();
-    let id = format!("execute/fib/recursive/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_fib_recursive(c: &mut Criterion) {
-    const INPUT: i64 = 30;
-    for vm in vms() {
-        run_fib_recursive(c, &*vm, INPUT);
-    }
-}
-
-fn run_fib_iterative(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().fib_iterative {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/fib.iterative.wat");
-    let name = vm.name();
-    let id = format!("execute/fib/iterative/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_fib_iterative(c: &mut Criterion) {
-    const INPUT: i64 = 2_000_000;
-    for vm in vms() {
-        run_fib_iterative(c, &*vm, INPUT);
-    }
-}
-
-fn run_fib_tailrec(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().fib_tailrec {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/fib.tailrec.wat");
-    let name = vm.name();
-    let id = format!("execute/fib/tailrec/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_fib_tailrec(c: &mut Criterion) {
-    const INPUT: i64 = 1_000_000;
-    for vm in vms() {
-        run_fib_tailrec(c, &*vm, INPUT);
-    }
-}
-
-fn run_primes(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().primes {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/primes.wat");
-    let name = vm.name();
-    let id = format!("execute/primes/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_primes(c: &mut Criterion) {
-    const INPUT: i64 = 1_000;
-    for vm in vms() {
-        run_primes(c, &*vm, INPUT);
-    }
-}
-
-fn run_matrix_multiply(c: &mut Criterion, vm: &dyn BenchVm, input: i64) {
-    if !vm.test_filter().matrix_multiply {
-        return;
-    }
-    static WASM: &[u8] = include_bytes!("../res/wat/matrix-multiplication.wat");
-    let name = vm.name();
-    let id = format!("execute/matmul/{name}/{input}");
-    c.bench_function(&id, |b| {
-        let wasm = wat2wasm(WASM);
-        let mut runtime = vm.load(&wasm[..]);
-        b.iter(|| {
-            runtime.call(input);
-        });
-    });
-}
-
-fn bench_matrix_multiply(c: &mut Criterion) {
-    const INPUT: i64 = 200;
-    for vm in vms() {
-        run_matrix_multiply(c, &*vm, INPUT);
-    }
 }
