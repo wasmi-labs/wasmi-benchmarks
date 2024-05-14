bd0528bcf02edd1bfe479bcf3d4ba110152abf4d
Robin Freyler <robin.freyler@gmail.com>
2024-05-14 17:14:16 +0200

    write coremark scores to file

--- src/coremark.rs
+++ src/coremark.rs
@@ -1,13 +1,17 @@
-use std::io::Write;
+use std::{collections::BTreeMap, io::Write};
 use wasmi_benchmarks::{read_benchmark_file, vms_under_test, InputEncoding};
 
 fn main() {
     let coremark_wasm = read_benchmark_file(InputEncoding::Wasm, "coremark-minimal");
+    let mut scores = <BTreeMap<String, f32>>::new();
     for vm in vms_under_test() {
         let name = vm.name();
         print!("Running Coremark 1.0\n\tusing {name} ");
         std::io::stdout().flush().unwrap();
-        let result = vm.coremark(&coremark_wasm[..]);
-        println!("\n\tresult = {result}\n");
+        let score = vm.coremark(&coremark_wasm[..]);
+        scores.insert(name.into(), score);
+        println!("\n\tscore = {score}\n");
     }
+    let json = serde_json::to_string(&scores).unwrap();
+    std::fs::write("coremark-scores.json", json).unwrap();
 }
