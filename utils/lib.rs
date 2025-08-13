c6f6524ecdb84f9c9ffdb8580ac9ea7b4a0c5a40
Robin Freyler <robin.freyler@gmail.com>
2025-08-13 14:03:54 +0200

    add benchmark-utils crate

---
+++ utils/lib.rs
@@ -0,0 +1,122 @@
+use wasmi_new::ModuleImportsIter;
+
+/// A Wasm runtime that is capable of being benchmarked.
+pub trait BenchRuntime {
+    /// Returns the name of the Wasm runtime and its configuration.
+    fn name(&self) -> &'static str;
+
+    /// Returns the [`TestFilter`] which applies to the Wasm runtime and its configuration.
+    fn test_filter(&self) -> TestFilter {
+        TestFilter::default()
+    }
+
+    /// Compiles the `wasm` using the Wasm runtime and its configuration.
+    fn compile(&self, wasm: &[u8], imports: ModuleImportsIter);
+
+    /// Loads a Wasm module instance using the Wasm runtime and its configuration.
+    ///
+    /// The returned Wasm module instance can then be used to issue calls.
+    fn load(&self, wasm: &[u8]) -> Box<dyn BenchInstance>;
+
+    /// Runs the given Coremark Wasm test and returns the result.
+    fn coremark(&self, wasm: &[u8]) -> f32;
+}
+
+/// The module instance of a Wasm runtime that is capable of being benchmarked.
+pub trait BenchInstance {
+    /// Calls the callable Wasm runtime module instance.
+    fn call(&mut self, input: i64);
+}
+
+/// Used to query elapsed time since last time this has been called. Used for Coremark benchmark.
+pub fn elapsed_ms() -> u32 {
+    use std::time::Instant;
+    static STARTED: std::sync::OnceLock<Instant> = std::sync::OnceLock::new();
+    let elapsed = STARTED.get_or_init(Instant::now).elapsed();
+    elapsed.as_millis() as u32
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct TestFilter {
+    pub execute: ExecuteTestFilter,
+    pub compile: CompileTestFilter,
+}
+
+impl TestFilter {
+    pub fn set_to(flag: bool) -> Self {
+        Self {
+            execute: ExecuteTestFilter::set_to(flag),
+            compile: CompileTestFilter::set_to(flag),
+        }
+    }
+}
+
+impl Default for TestFilter {
+    fn default() -> Self {
+        Self::set_to(true)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct ExecuteTestFilter {
+    pub counter: bool,
+    pub fib_iterative: bool,
+    pub fib_recursive: bool,
+    pub fib_tailrec: bool,
+    pub primes: bool,
+    pub matrix_multiply: bool,
+    pub argon2: bool,
+    pub bulk_ops: bool,
+    pub coremark: bool,
+}
+
+impl ExecuteTestFilter {
+    pub fn set_to(flag: bool) -> Self {
+        Self {
+            counter: flag,
+            fib_iterative: flag,
+            fib_recursive: flag,
+            fib_tailrec: flag,
+            primes: flag,
+            matrix_multiply: flag,
+            argon2: flag,
+            bulk_ops: flag,
+            coremark: flag,
+        }
+    }
+}
+
+impl Default for ExecuteTestFilter {
+    fn default() -> Self {
+        Self::set_to(true)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct CompileTestFilter {
+    pub bz2: bool,
+    pub pulldown_cmark: bool,
+    pub spidermonkey: bool,
+    pub ffmpeg: bool,
+    pub coremark_minimal: bool,
+    pub argon2: bool,
+}
+
+impl CompileTestFilter {
+    pub fn set_to(flag: bool) -> Self {
+        Self {
+            bz2: flag,
+            pulldown_cmark: flag,
+            spidermonkey: flag,
+            ffmpeg: flag,
+            coremark_minimal: flag,
+            argon2: flag,
+        }
+    }
+}
+
+impl Default for CompileTestFilter {
+    fn default() -> Self {
+        Self::set_to(true)
+    }
+}
