2bcb1a00bd1e7abb9c71b45f6ec5629e78e062e2
Robin Freyler <robin.freyler@gmail.com>
2025-12-03 12:21:05 +0100

    use Wasmi 1.0

--- src/lib.rs
+++ src/lib.rs
@@ -11,43 +11,43 @@ pub fn vms_under_test() -> Vec<Box<dyn BenchRuntime>> {
             compilation_mode: rt_wasmi::CompilationMode::Eager,
             validation: rt_wasmi::Validation::Checked,
         }),
-        Box::new(rt_wasmi::Wasmi {
-            compilation_mode: rt_wasmi::CompilationMode::LazyTranslation,
-            validation: rt_wasmi::Validation::Checked,
-        }),
-        Box::new(rt_wasmi::Wasmi {
-            compilation_mode: rt_wasmi::CompilationMode::Lazy,
-            validation: rt_wasmi::Validation::Checked,
-        }),
-        Box::new(rt_wasmi::Wasmi {
-            compilation_mode: rt_wasmi::CompilationMode::Lazy,
-            validation: rt_wasmi::Validation::Unchecked,
-        }),
-        Box::new(rt_tinywasm::Tinywasm),
-        Box::new(rt_wasm3::Wasm3 {
-            compilation_mode: rt_wasm3::CompilationMode::Eager,
-        }),
-        Box::new(rt_wasm3::Wasm3 {
-            compilation_mode: rt_wasm3::CompilationMode::Lazy,
-        }),
-        Box::new(rt_stitch::Stitch),
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Cranelift,
-        }),
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Winch,
-        }),
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Pulley,
-        }),
-        Box::new(rt_wasmer::Wasmer {
-            compiler: rt_wasmer::WasmerCompiler::Cranelift,
-        }),
-        Box::new(rt_wasmer::Wasmer {
-            compiler: rt_wasmer::WasmerCompiler::Singlepass,
-        }),
-        Box::new(rt_wasmer::Wasmer {
-            compiler: rt_wasmer::WasmerCompiler::Wamr,
-        }),
+        // Box::new(rt_wasmi::Wasmi {
+        //     compilation_mode: rt_wasmi::CompilationMode::LazyTranslation,
+        //     validation: rt_wasmi::Validation::Checked,
+        // }),
+        // Box::new(rt_wasmi::Wasmi {
+        //     compilation_mode: rt_wasmi::CompilationMode::Lazy,
+        //     validation: rt_wasmi::Validation::Checked,
+        // }),
+        // Box::new(rt_wasmi::Wasmi {
+        //     compilation_mode: rt_wasmi::CompilationMode::Lazy,
+        //     validation: rt_wasmi::Validation::Unchecked,
+        // }),
+        // Box::new(rt_tinywasm::Tinywasm),
+        // Box::new(rt_wasm3::Wasm3 {
+        //     compilation_mode: rt_wasm3::CompilationMode::Eager,
+        // }),
+        // Box::new(rt_wasm3::Wasm3 {
+        //     compilation_mode: rt_wasm3::CompilationMode::Lazy,
+        // }),
+        // Box::new(rt_stitch::Stitch),
+        // Box::new(rt_wasmtime::Wasmtime {
+        //     strategy: rt_wasmtime::Strategy::Cranelift,
+        // }),
+        // Box::new(rt_wasmtime::Wasmtime {
+        //     strategy: rt_wasmtime::Strategy::Winch,
+        // }),
+        // Box::new(rt_wasmtime::Wasmtime {
+        //     strategy: rt_wasmtime::Strategy::Pulley,
+        // }),
+        // Box::new(rt_wasmer::Wasmer {
+        //     compiler: rt_wasmer::WasmerCompiler::Cranelift,
+        // }),
+        // Box::new(rt_wasmer::Wasmer {
+        //     compiler: rt_wasmer::WasmerCompiler::Singlepass,
+        // }),
+        // Box::new(rt_wasmer::Wasmer {
+        //     compiler: rt_wasmer::WasmerCompiler::Wamr,
+        // }),
     ]
 }
