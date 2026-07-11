ffb36d62f5c3532772e8834355a391aa164f4ea3
Robin Freyler <robin.freyler@gmail.com>
2026-07-11 13:13:12 +0200

    move runtime dependencies in each of the sub-crates

--- runtimes/wasmi-032/lib.rs
+++ runtimes/wasmi-032/lib.rs
@@ -5,9 +5,9 @@ use benchmark_utils::{BenchInstance, BenchRuntime, ModuleImportsIter, elapsed_ms
 pub struct Wasmi032;
 
 struct Wasmi032Runtime {
-    store: wasmi032::Store<()>,
-    _instance: wasmi032::Instance,
-    func: wasmi032::TypedFunc<i64, i64>,
+    store: wasmi::Store<()>,
+    _instance: wasmi::Instance,
+    func: wasmi::TypedFunc<i64, i64>,
 }
 
 impl BenchRuntime for Wasmi032 {
@@ -17,14 +17,14 @@ impl BenchRuntime for Wasmi032 {
 
     fn compile(&self, wasm: &[u8], _imports: ModuleImportsIter) {
         let store = self.store();
-        wasmi032::Module::new(store.engine(), wasm).unwrap();
+        wasmi::Module::new(store.engine(), wasm).unwrap();
     }
 
     fn load(&self, wasm: &[u8]) -> Box<dyn BenchInstance> {
         let mut store = self.store();
         let engine = store.engine();
-        let module = wasmi032::Module::new(engine, wasm).unwrap();
-        let linker = wasmi032::Linker::new(engine);
+        let module = wasmi::Module::new(engine, wasm).unwrap();
+        let linker = wasmi::Linker::new(engine);
         let instance = linker
             .instantiate(&mut store, &module)
             .unwrap()
@@ -39,19 +39,19 @@ impl BenchRuntime for Wasmi032 {
     }
 
     fn coremark(&self, wasm: &[u8]) -> f32 {
-        let engine = wasmi032::Engine::default();
-        let mut store = <wasmi032::Store<()>>::new(&engine, ());
-        let mut linker = wasmi032::Linker::new(store.engine());
+        let engine = wasmi::Engine::default();
+        let mut store = <wasmi::Store<()>>::new(&engine, ());
+        let mut linker = wasmi::Linker::new(store.engine());
         linker
             .func_wrap("env", "clock_ms", || elapsed_ms() as i32)
             .unwrap();
-        let module = wasmi032::Module::new(store.engine(), wasm).unwrap();
+        let module = wasmi::Module::new(store.engine(), wasm).unwrap();
         let result = linker
             .instantiate(&mut store, &module)
             .unwrap()
             .ensure_no_start(&mut store)
             .unwrap()
-            .get_typed_func::<(), wasmi032::core::F32>(&mut store, "run")
+            .get_typed_func::<(), wasmi::core::F32>(&mut store, "run")
             .unwrap()
             .call(&mut store, ())
             .unwrap();
@@ -60,11 +60,11 @@ impl BenchRuntime for Wasmi032 {
 }
 
 impl Wasmi032 {
-    fn store(&self) -> wasmi032::Store<()> {
-        let mut config = wasmi032::Config::default();
+    fn store(&self) -> wasmi::Store<()> {
+        let mut config = wasmi::Config::default();
         config.wasm_tail_call(true);
-        let engine = wasmi032::Engine::new(&config);
-        <wasmi032::Store<()>>::new(&engine, ())
+        let engine = wasmi::Engine::new(&config);
+        <wasmi::Store<()>>::new(&engine, ())
     }
 }
 
