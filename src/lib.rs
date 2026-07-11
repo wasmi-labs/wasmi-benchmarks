b475b45bc886f46359180ba65f3a4d58c39abb8f
Robin Freyler <robin.freyler@gmail.com>
2026-07-11 16:49:37 +0200

    clean-up src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,76 +4,85 @@ use benchmark_utils::BenchRuntime;
 
 /// Returns the Wasm runtimes with a set of configurations to test.
 pub fn vms_under_test() -> Vec<Box<dyn BenchRuntime>> {
-    vec![
-        #[cfg(feature = "wasmi-v0-31")]
-        Box::new(rt_wasmi_v0_31::WasmiV031),
-        #[cfg(feature = "wasmi-v0-32")]
-        Box::new(rt_wasmi_v0_32::WasmiV032),
-        #[cfg(feature = "wasmi-v1")]
-        Box::new(rt_wasmi_v1::Wasmi {
-            compilation_mode: rt_wasmi_v1::CompilationMode::Eager,
-            validation: rt_wasmi_v1::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v1")]
-        Box::new(rt_wasmi_v1::Wasmi {
-            compilation_mode: rt_wasmi_v1::CompilationMode::LazyTranslation,
-            validation: rt_wasmi_v1::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v1")]
-        Box::new(rt_wasmi_v1::Wasmi {
-            compilation_mode: rt_wasmi_v1::CompilationMode::Lazy,
-            validation: rt_wasmi_v1::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v1")]
-        Box::new(rt_wasmi_v1::Wasmi {
-            compilation_mode: rt_wasmi_v1::CompilationMode::Lazy,
-            validation: rt_wasmi_v1::Validation::Unchecked,
-        }),
-        #[cfg(feature = "wasmi-v2")]
-        Box::new(rt_wasmi_v2::Wasmi {
-            compilation_mode: rt_wasmi_v2::CompilationMode::Eager,
-            validation: rt_wasmi_v2::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v2")]
-        Box::new(rt_wasmi_v2::Wasmi {
-            compilation_mode: rt_wasmi_v2::CompilationMode::LazyTranslation,
-            validation: rt_wasmi_v2::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v2")]
-        Box::new(rt_wasmi_v2::Wasmi {
-            compilation_mode: rt_wasmi_v2::CompilationMode::Lazy,
-            validation: rt_wasmi_v2::Validation::Checked,
-        }),
-        #[cfg(feature = "wasmi-v2")]
-        Box::new(rt_wasmi_v2::Wasmi {
-            compilation_mode: rt_wasmi_v2::CompilationMode::Lazy,
-            validation: rt_wasmi_v2::Validation::Unchecked,
-        }),
-        #[cfg(feature = "tinywasm")]
-        Box::new(rt_tinywasm::Tinywasm),
-        #[cfg(feature = "wasm3")]
-        Box::new(rt_wasm3::Wasm3),
-        #[cfg(feature = "stitch")]
-        Box::new(rt_stitch::Stitch),
-        #[cfg(feature = "wasmtime-cranelift")]
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Cranelift,
-        }),
-        #[cfg(feature = "wasmtime-winch")]
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Winch,
-        }),
-        #[cfg(feature = "wasmtime-pulley")]
-        Box::new(rt_wasmtime::Wasmtime {
-            strategy: rt_wasmtime::Strategy::Pulley,
-        }),
-        #[cfg(feature = "wasmer-cranelift")]
-        Box::new(rt_wasmer::Wasmer {
-            compiler: rt_wasmer::WasmerCompiler::Cranelift,
-        }),
-        #[cfg(feature = "wasmer-singlepass")]
-        Box::new(rt_wasmer::Wasmer {
-            compiler: rt_wasmer::WasmerCompiler::Singlepass,
-        }),
-    ]
+    let mut rts = Rts::default();
+    #[cfg(feature = "wasmi-v0-31")]
+    rts.push(rt_wasmi_v0_31::WasmiV031);
+    #[cfg(feature = "wasmi-v0-32")]
+    rts.push(rt_wasmi_v0_32::WasmiV032);
+    #[cfg(feature = "wasmi-v1")]
+    push_wasmi_v1_rts(&mut rts);
+    #[cfg(feature = "wasmi-v2")]
+    push_wasmi_v2_rts(&mut rts);
+    #[cfg(feature = "tinywasm")]
+    rts.push(rt_tinywasm::Tinywasm);
+    #[cfg(feature = "wasm3")]
+    rts.push(rt_wasm3::Wasm3);
+    #[cfg(feature = "stitch")]
+    rts.push(rt_stitch::Stitch);
+    #[cfg(feature = "wasmtime-cranelift")]
+    rts.push(rt_wasmtime::Wasmtime {
+        strategy: rt_wasmtime::Strategy::Cranelift,
+    });
+    #[cfg(feature = "wasmtime-winch")]
+    rts.push(rt_wasmtime::Wasmtime {
+        strategy: rt_wasmtime::Strategy::Winch,
+    });
+    #[cfg(feature = "wasmtime-pulley")]
+    rts.push(rt_wasmtime::Wasmtime {
+        strategy: rt_wasmtime::Strategy::Pulley,
+    });
+    #[cfg(feature = "wasmer-cranelift")]
+    rts.push(rt_wasmer::Wasmer {
+        compiler: rt_wasmer::WasmerCompiler::Cranelift,
+    });
+    #[cfg(feature = "wasmer-singlepass")]
+    rts.push(rt_wasmer::Wasmer {
+        compiler: rt_wasmer::WasmerCompiler::Singlepass,
+    });
+    rts.into_vec()
+}
+
+#[derive(Default)]
+struct Rts(Vec<Box<dyn BenchRuntime>>);
+
+impl Rts {
+    fn push(&mut self, rt: impl BenchRuntime + 'static) {
+        self.0.push(Box::new(rt));
+    }
+
+    fn into_vec(self) -> Vec<Box<dyn BenchRuntime>> {
+        self.0
+    }
+}
+
+#[cfg(feature = "wasmi-v1")]
+fn push_wasmi_v1_rts(rts: &mut Rts) {
+    use rt_wasmi_v1::{CompilationMode, Validation};
+    for (compilation_mode, validation) in [
+        (CompilationMode::Eager, Validation::Checked),
+        (CompilationMode::LazyTranslation, Validation::Checked),
+        (CompilationMode::Lazy, Validation::Checked),
+        (CompilationMode::Lazy, Validation::Unchecked),
+    ] {
+        rts.push(rt_wasmi_v1::Wasmi {
+            compilation_mode,
+            validation,
+        });
+    }
+}
+
+#[cfg(feature = "wasmi-v2")]
+fn push_wasmi_v2_rts(rts: &mut Rts) {
+    use rt_wasmi_v1::{CompilationMode, Validation};
+    for (compilation_mode, validation) in [
+        (CompilationMode::Eager, Validation::Checked),
+        (CompilationMode::LazyTranslation, Validation::Checked),
+        (CompilationMode::Lazy, Validation::Checked),
+        (CompilationMode::Lazy, Validation::Unchecked),
+    ] {
+        rts.push(rt_wasmi_v1::Wasmi {
+            compilation_mode,
+            validation,
+        });
+    }
 }
