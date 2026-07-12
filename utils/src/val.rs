15ce0ba08c4e9a4697f9aaa90dc5059353271bc8
Robin Freyler <robin.freyler@gmail.com>
2026-07-12 19:06:59 +0200

    add Display impl for utils Val type

--- utils/src/val.rs
+++ utils/src/val.rs
@@ -13,6 +13,17 @@ pub enum Val {
     F64(f64),
 }
 
+impl fmt::Display for Val {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Val::I32(val) => val.fmt(f),
+            Val::I64(val) => val.fmt(f),
+            Val::F32(val) => val.fmt(f),
+            Val::F64(val) => val.fmt(f),
+        }
+    }
+}
+
 impl Val {
     /// Returns the [`ValType`] of `self`.
     #[inline]
