4523f5e78d4c6d88b356351cd0346b6f85382b9c
Robin Freyler <robin.freyler@gmail.com>
2026-07-13 13:38:26 +0200

    implement new trait for wasmer

--- runtimes/wasmer/lib.rs
+++ runtimes/wasmer/lib.rs
@@ -1,28 +1,37 @@
 #![crate_type = "dylib"]
 
-use benchmark_utils as utils;
-use benchmark_utils::{BenchInstance, BenchRuntime, ExecuteTestId, TestId};
-use wasmer::Function;
+use benchmark_utils::{self as utils, CompileTestId};
+use benchmark_utils::{ExecuteTestId, ModuleInstance, Runtime, RuntimeInstance, TestId};
 use wasmer::Type as ValType;
 use wasmer::Value as Val;
 
-pub struct Wasmer {
-    pub compiler: WasmerCompiler,
-}
-
+#[derive(Debug, Copy, Clone)]
 pub enum WasmerCompiler {
     Cranelift,
     Singlepass,
 }
 
-struct WasmerRuntime {
+pub struct Wasmer {
+    pub compiler: WasmerCompiler,
+}
+
+/// A concrete Wasmer runtime with its store and imports, produced by [`Wasmer::setup`].
+///
+/// Wasmer binds host functions to a [`Store`](wasmer::Store), so the store is created up front
+/// and the module that [`RuntimeInstance::load`] instantiates runs in this same store.
+struct WasmerInstance {
+    store: wasmer::Store,
+    imports: wasmer::Imports,
+}
+
+/// An instantiated Wasmer module, produced by [`WasmerInstance::load`].
+struct WasmerModule {
     store: wasmer::Store,
     instance: wasmer::Instance,
     params: Vec<Val>,
-    results: Vec<Val>,
 }
 
-impl BenchRuntime for Wasmer {
+impl Runtime for Wasmer {
     fn id(&self) -> &'static str {
         match self.compiler {
             WasmerCompiler::Cranelift => "wasmer.cranelift",
@@ -30,67 +39,88 @@ impl BenchRuntime for Wasmer {
         }
     }
 
-    fn can_run(&self, id: TestId) -> bool {
-        !matches!(id, TestId::Execute(ExecuteTestId::FibonacciTail))
-    }
-
-    fn compile(&self, wasm: &[u8]) {
-        let store = self.store();
+    fn compile(&self, id: CompileTestId, wasm: &[u8]) -> bool {
+        if !self.can_run(id.into()) {
+            return false;
+        }
+        let store = make_store(self.compiler);
         wasmer::Module::new(&store, wasm).unwrap();
+        true
     }
 
-    fn load(&self, wasm: &[u8]) -> Box<dyn BenchInstance> {
-        let mut store = self.store();
-        let module = wasmer::Module::new(&store, wasm).unwrap();
-        let import_object = wasmer::imports! {};
-        let instance = wasmer::Instance::new(&mut store, &module, &import_object).unwrap();
-        Box::new(WasmerRuntime {
-            store,
-            instance,
-            params: Vec::new(),
-            results: Vec::new(),
-        })
-    }
-
-    fn coremark(&self, wasm: &[u8], elapsed_ms: fn() -> u32) -> f32 {
-        let mut store = self.store();
-        let module = wasmer::Module::new(&store, wasm).unwrap();
-        let import_object = wasmer::imports! {
-            "env" => {
-                "clock_ms" => wasmer::Function::new_typed(&mut store, elapsed_ms),
-            }
-        };
-        let instance = wasmer::Instance::new(&mut store, &module, &import_object).unwrap();
-        instance
-            .exports
-            .get_typed_function::<(), f32>(&store, "run")
-            .unwrap()
-            .call(&mut store)
-            .unwrap()
+    fn setup(&self, id: TestId) -> Option<Box<dyn RuntimeInstance>> {
+        if !self.can_run(id) {
+            return None;
+        }
+        Some(Box::new(WasmerInstance {
+            store: make_store(self.compiler),
+            imports: wasmer::Imports::new(),
+        }))
     }
 }
 
 impl Wasmer {
-    fn store(&self) -> wasmer::Store {
-        match self.compiler {
-            WasmerCompiler::Cranelift => {
-                let builder =
-                    wasmer::sys::EngineBuilder::new(wasmer_compiler_cranelift::Cranelift::new());
-                let mut features = wasmer::sys::Features::new();
-                features.tail_call(true);
-                let engine = builder.set_features(Some(features)).engine();
-                wasmer::Store::new(engine)
-            }
-            WasmerCompiler::Singlepass => {
-                let builder =
-                    wasmer::sys::EngineBuilder::new(wasmer_compiler_singlepass::Singlepass::new());
-                wasmer::Store::new(builder)
-            }
+    fn can_run(&self, id: TestId) -> bool {
+        !matches!(id, TestId::Execute(ExecuteTestId::FibonacciTail))
+    }
+}
+
+impl RuntimeInstance for WasmerInstance {
+    fn link_func(
+        &mut self,
+        module: &str,
+        name: &str,
+        ty: utils::FuncType,
+        func: fn(params: &[utils::Val], results: &mut [utils::Val]),
+    ) {
+        let result_tys: Vec<utils::ValType> = ty.results().to_vec();
+        let params: Vec<ValType> = ty.params().iter().copied().map(to_wasmer_type).collect();
+        let results: Vec<ValType> = ty.results().iter().copied().map(to_wasmer_type).collect();
+        let ty = wasmer::FunctionType::new(params, results);
+        let host = wasmer::Function::new(&mut self.store, ty, move |args: &[Val]| {
+            let in_params: Vec<utils::Val> = args.iter().cloned().map(into_utils_val).collect();
+            let mut out: Vec<utils::Val> = result_tys
+                .iter()
+                .copied()
+                .map(utils::Val::default_for_ty)
+                .collect();
+            func(&in_params, &mut out);
+            Ok(out.into_iter().map(from_utils_val).collect())
+        });
+        self.imports.define(module, name, host);
+    }
+
+    fn instantiate(self: Box<Self>, wasm: &[u8]) -> Box<dyn ModuleInstance> {
+        let WasmerInstance { mut store, imports } = *self;
+        let module = wasmer::Module::new(&store, wasm).unwrap();
+        let instance = wasmer::Instance::new(&mut store, &module, &imports).unwrap();
+        Box::new(WasmerModule {
+            store,
+            instance,
+            params: Vec::new(),
+        })
+    }
+}
+
+fn make_store(compiler: WasmerCompiler) -> wasmer::Store {
+    match compiler {
+        WasmerCompiler::Cranelift => {
+            let builder =
+                wasmer::sys::EngineBuilder::new(wasmer_compiler_cranelift::Cranelift::new());
+            let mut features = wasmer::sys::Features::new();
+            features.tail_call(true);
+            let engine = builder.set_features(Some(features)).engine();
+            wasmer::Store::new(engine)
+        }
+        WasmerCompiler::Singlepass => {
+            let builder =
+                wasmer::sys::EngineBuilder::new(wasmer_compiler_singlepass::Singlepass::new());
+            wasmer::Store::new(builder)
         }
     }
 }
 
-impl BenchInstance for WasmerRuntime {
+impl ModuleInstance for WasmerModule {
     fn call(
         &mut self,
         name: &str,
@@ -101,42 +131,33 @@ impl BenchInstance for WasmerRuntime {
         assert_eq!(params.len(), func.ty(&self.store).params().len());
         assert_eq!(results.len(), func.ty(&self.store).results().len());
         self.prepare_params(params);
-        self.prepare_results(&func);
         let call_results = func.call(&mut self.store, &self.params[..])?;
-        self.write_back_results(results, call_results);
+        self.write_back_results(results, &call_results);
         Ok(())
     }
 }
 
-impl WasmerRuntime {
+impl WasmerModule {
     fn prepare_params(&mut self, params: &[utils::Val]) {
         self.params.clear();
         self.params
             .extend(params.iter().copied().map(from_utils_val));
     }
 
-    fn prepare_results(&mut self, func: &Function) {
-        self.results.clear();
-        for ty in func.ty(&self.store).results() {
-            self.results.push(default_val(*ty))
-        }
-    }
-
-    fn write_back_results(&self, dst: &mut [utils::Val], src: Box<[Val]>) {
+    fn write_back_results(&self, dst: &mut [utils::Val], src: &[Val]) {
         assert_eq!(dst.len(), src.len());
         for (dst, src) in dst.iter_mut().zip(src) {
-            *dst = into_utils_val(src);
+            *dst = into_utils_val(src.clone());
         }
     }
 }
 
-fn default_val(ty: ValType) -> Val {
+fn to_wasmer_type(ty: utils::ValType) -> ValType {
     match ty {
-        ValType::I32 => Val::I32(0),
-        ValType::I64 => Val::I64(0),
-        ValType::F32 => Val::F32(0.0),
-        ValType::F64 => Val::F64(0.0),
-        _ => unreachable!(),
+        utils::ValType::I32 => ValType::I32,
+        utils::ValType::I64 => ValType::I64,
+        utils::ValType::F32 => ValType::F32,
+        utils::ValType::F64 => ValType::F64,
     }
 }
 
