ea321ff8ec2909701381e95366b6f01b867d6300
Robin Freyler <robin.freyler@gmail.com>
2026-07-13 16:57:30 +0200

    add Display impl for TestId

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -97,6 +97,15 @@ impl From<ExecuteTestId> for TestId {
     }
 }
 
+impl fmt::Display for TestId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TestId::Startup(id) => id.fmt(f),
+            TestId::Execute(id) => id.fmt(f),
+        }
+    }
+}
+
 #[derive(Copy, Clone)]
 pub enum StartupTestId {
     Erc20,
@@ -106,6 +115,21 @@ pub enum StartupTestId {
     Ffmpeg,
     CoreMark,
     Argon2,
+}
+
+impl fmt::Display for StartupTestId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            StartupTestId::Erc20 => "erc20",
+            StartupTestId::Bz2 => "bz2",
+            StartupTestId::PulldownCmark => "pulldown-cmark",
+            StartupTestId::Spidermonkey => "spidermonkey",
+            StartupTestId::Ffmpeg => "ffmpeg",
+            StartupTestId::CoreMark => "coremark",
+            StartupTestId::Argon2 => "argon2",
+        };
+        f.write_str(s)
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -120,6 +144,24 @@ pub enum ExecuteTestId {
     Argon2,
     BulkOps,
     CoreMark,
+}
+
+impl fmt::Display for ExecuteTestId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            ExecuteTestId::CounterLocal => "counter-local",
+            ExecuteTestId::CounterParam => "counter-param",
+            ExecuteTestId::FibonacciIter => "fibonacci-rec",
+            ExecuteTestId::FibonacciRec => "fibonacci-iter",
+            ExecuteTestId::FibonacciTail => "fibonacci-tail",
+            ExecuteTestId::Primes => "primes",
+            ExecuteTestId::MatrixMultiply => "matmul",
+            ExecuteTestId::Argon2 => "argon2",
+            ExecuteTestId::BulkOps => "bulk-ops",
+            ExecuteTestId::CoreMark => "coremark",
+        };
+        f.write_str(s)
+    }
 }
 
 /// Converts the `.wat` encoded `bytes` into `.wasm` encoded bytes.
